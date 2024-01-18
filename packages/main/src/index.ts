b95b7a5ac08ebca7314a74584f0051af541128d7
Bruno Tot <brunotot10000@gmail.com>
2024-01-18 18:31:56 +0100

    feat: add subtract function

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -7,3 +7,13 @@
 export function add(a: number, b: number) {
   return a + b;
 }
+
+/**
+ * Subtracts two numbers.
+ * @param a - The first number.
+ * @param b - The second number.
+ * @returns The difference of the two numbers.
+ */
+export function subtract(a: number, b: number) {
+  return a - b;
+}
