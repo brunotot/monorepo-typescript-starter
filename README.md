8559df9681574535dee9df190f03ac529c1ce467
Bruno Tot <brunotot10000@gmail.com>
2024-01-19 11:50:32 +0100

    docs: add getting started section to readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Hello world
+## Getting started
+
+```sh
+curl -sL https://raw.githubusercontent.com/brunotot/monorepo-typescript-starter/main/init.sh | bash -s -- [REPO_NAME]
+```
---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+REPO_SRC="https://github.com/brunotot/monorepo-typescript-starter.git"
+REPO_NAME="$1"
+git clone "$REPO_SRC" "$REPO_NAME"
+cd "./$REPO_NAME"
+rm -rf .git
+git init
+if command -v code >/dev/null 2>&1; then
+    code .
+fi
