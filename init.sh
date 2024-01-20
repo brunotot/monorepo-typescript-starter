06ff2c1f67faf6806552ef9e94d23bd592418ca7
Isus Krist <isuskrist10000@gmail.com>
2024-01-20 14:04:54 +0100

    chore: delete init.sh

--- init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-REPO_SRC="https://github.com/brunotot/monorepo-typescript-starter.git"
-REPO_NAME="$1"
-git clone "$REPO_SRC" "$REPO_NAME"
-cd "./$REPO_NAME"
-rm -rf .git
-git init
-pnpm install
